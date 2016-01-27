e98e2d78b9fa3cfbcc374c40e2996069cb875f31
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-27 11:16:34 -0500

    Initial Commit

---
+++ instaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instaKilo
+//
+//  Created by Kerry Toonen on 2016-01-27.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instaKilo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instaKilo
+//
+//  Created by Kerry Toonen on 2016-01-27.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
